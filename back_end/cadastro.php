d40cd2635f670ec424f3727acca32e64bb113a13
pedro-6969 <pedromiguel19712@gmail.com>
2025-11-27 14:15:48 -0300

    Definição das cores

--- back_end/cadastro.php
+++ back_end/cadastro.php
@@ -1,10 +1,23 @@
 <?php
-    include '/front_end/assets/header.html';
+    include 'Api/cors.php';
+    include 'Api/conexao.php';
+    include '../../front_end/assets/header.html';
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $
+
+    }
 ?>
 
 
 <main>
-
+    <form action="cadastro.php" method="POST">
+        <label for="email"></label>
+        <label for="usuario"></label>
+        <label for="senha"></label>
+        <label for="confirmar_senha"></label>
+    </form>
 </main>
 
 
