565cf885ae8f2b20a9e1459cd6c5458709567234
pedro-6969 <pedromiguel19712@gmail.com>
2025-12-02 15:47:29 -0300

    admin

--- back_end/admin.php
+++ back_end/admin.php
@@ -1,7 +1,26 @@
-<?php include '../front_end/header.html'; ?>
-<?php include 'Api/conexao.php' ?>
+<?php
+    include 'Api/cors.php';
+    include 'Api/conexao.php';
+    include '../front_end/assets/header.php';
+?>
 
+<main>
+    <button id="abrir-modal">Adicionar Post</button>
+    <div class="hidden">
+        <form action="admin.php" method="POST" enctype="multipart/form-data">
 
+            <label for="titulo">Título:</label>
+            <input type="text" name="titulo" id="titulo" required>
+
+            <label for="conteudo">Conteúdo:</label>
+            <input type="text" name="conteudo" id="titulo" required>
+
+            <label for="thumb">Thumb:</label>
+            <input type="file" name="thumb" id="thumb">
+
+        </form>
+    </div>
+</main>
 
 <?php
 session_start();
@@ -12,7 +31,4 @@ if (!isset($_SESSION['id'])) {
 }
 ?>
 
-
-
-
-<?php include '../front_end/footer.html'; ?>
+<?php include '../front_end/assets/footer.php'; ?>
